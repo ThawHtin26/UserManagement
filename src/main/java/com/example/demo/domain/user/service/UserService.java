5a68d696dd4f8e2ceefdebf00f981382cc26f1ba
Thaw Htin Aung <thawhtinaung26@gmail.com>
2022-08-02 16:53:12 +0630

    MyBatis initialization.

---
+++ src/main/java/com/example/demo/domain/user/service/UserService.java
@@ -0,0 +1,5 @@
+package com.example.demo.domain.user.service;
+
+public class UserService {
+
+}
---
+++ src/main/java/com/example/demo/domain/user/service/impl/UserServiceImpl.java
@@ -0,0 +1,5 @@
+package com.example.demo.domain.user.service.impl;
+
+public class UserServiceImpl {
+
+}
