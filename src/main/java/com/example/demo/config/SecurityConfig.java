9bfac57094f51b13605bcf5a89566fe3913a15ef
Thaw Htin Aung <thawhtinaung26@gmail.com>
2022-08-03 12:27:44 +0630

    Logout function has implmented.

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @EnableWebSecurity
 @Configuration
@@ -46,6 +47,11 @@ public class SecurityConfig {
 				.usernameParameter("userId")
 				.passwordParameter("password")
 				.defaultSuccessUrl("/user/list",true);
+		
+		http.logout()
+			.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+			.logoutUrl("/logout")
+			.logoutSuccessUrl("/login?logout");
 		
 		http.csrf().disable();
 		return http.build();
